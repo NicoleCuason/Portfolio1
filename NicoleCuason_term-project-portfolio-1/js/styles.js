b6e3bf218730fb3ab705ea35f0828a195f14b990
NicoleCuason <ncc93celeb@yahoo.com>
2025-04-11 03:24:15 -0700

    Add files via upload

---
+++ NicoleCuason_term-project-portfolio-1/js/styles.js
@@ -0,0 +1,11 @@
+const mobileMenuButton = document.querySelector('.mobile-menu');
+const mobileNav = document.querySelector('.mobile-nav');
+
+// Toggle the mobile menu when the button is clicked
+mobileMenuButton.addEventListener('click', () => {
+  mobileNav.classList.toggle('active');
+});
+
+
+
+
